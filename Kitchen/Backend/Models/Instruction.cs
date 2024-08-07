677becab485c73d672a099abdd2c1b9ef0d9b8a7
rick <kadlecr@cngroup.dk>
2024-08-07 16:59:37 +0200

    Add instruction by dialog window;

---
+++ Kitchen/Backend/Models/Instruction.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kitchen.Backend.Models;
+
+[Owned]
+public class Instruction
+{
+    [Key]
+    public long Id { get; set; }
+    public int Number { get; set; }
+    public string Description { get; set; }
+}
